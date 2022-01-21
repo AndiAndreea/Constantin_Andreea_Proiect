ab593d8606ccf7bddfbf6df1878c20f542925d45
Constantin Andreea <andy_tkd@yahoo.com>
2022-01-21 08:58:13 +0200

    Add project files.

---
+++ Data/Constantin_Andreea_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Constantin_Andreea_Lab8.Models;
+
+namespace Constantin_Andreea_Lab8.Data
+{
+    public class Constantin_Andreea_Lab8Context : DbContext
+    {
+        public Constantin_Andreea_Lab8Context (DbContextOptions<Constantin_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Constantin_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Constantin_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Constantin_Andreea_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Constantin_Andreea_Lab8.Data;
+using Constantin_Andreea_Lab8.Models;
+
+namespace Constantin_Andreea_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Constantin_Andreea_Lab8.Data.Constantin_Andreea_Lab8Context _context;
+
+        public IndexModel(Constantin_Andreea_Lab8.Data.Constantin_Andreea_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
